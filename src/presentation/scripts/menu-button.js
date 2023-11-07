decbb90c94da25904a4925f27f736a478c8e61d9
RKOrtega94 <rkortega1994@gmail.com>
2023-11-07 14:35:42 -0500

    Refactor sidebar menu component and script

--- src/presentation/scripts/menu-button.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(["[menu-sidebar-button]"]);
-const menu = document.querySelector(["[menu-sidebar-options]"]);
-
-menu.classList.add("hidden");
-
-button.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  menu.classList.toggle("flex");
-});
