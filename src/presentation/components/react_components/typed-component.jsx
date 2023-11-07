e66567f601ff8521ef3ed2a5a36fc3a18a795301
RKOrtega94 <rkortega1994@gmail.com>
2023-11-07 17:30:44 -0500

    Add React integration and update tsconfig.json and package.json

---
+++ src/presentation/components/react_components/typed-component.jsx
@@ -0,0 +1,32 @@
+import { useRef, useEffect } from "react";
+
+import Typed from "typed.js";
+
+export default function TypedComponent(props) {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        "Web developer",
+        "Mobile developer",
+        "Fullstack developer",
+        "Database engineer",
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <span ref={el} />
+    </div>
+  );
+}
